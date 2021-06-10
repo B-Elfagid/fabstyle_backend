e6f74bdb0594351aeb156466009d9746d3e185b9
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-06-11 00:51:29 +0100

    add seed data & create and migrate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+clothing = Category.create(name: "Clothing")
+shoes = Category.create(name: "Shoes")
+acessories = Category.create(name: "Acessories")
+
+Post.create(brand: "newlook" image: dress.jpg, price: 10.99, size: 12, description: "red dress", website: "www.newlook.com", category: clothing)
+Post.create(brand: "nike" image: nike.jpg, price: 120.99, size: 5, description: "black nike air max", website: "www.nike.com", category: shoes)
+Post.create(brand: "accessorize" image: necklace.jpg, price: 60, size: 16 inch, description: "layered necklace, featuring faux gemstones and various pretty pendants.", website: "www.accessorize.com", category: acessories)
